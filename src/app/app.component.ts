f16e9357af0392841818c4431dae5c05a6419191
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2023-06-11 16:52:46 +0200

    install angular and add some code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Habit } from './models/habit';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'habit-tracker';
+  public habits: Habit[] = [
+    <Habit>{
+      name: '15 min walk',
+      frequency: 'daily',
+      description: 'This habit makes my kitchen look nice and makes my day better the next morning.'
+    },
+    <Habit>{
+      name: 'Weed the garden',
+      frequency: 'weekly',
+      description: 'The weeds get so out of hand if they wait any longer, and I like how nice our home looks with a clean lawn.'
+    }
+  ];
 }
---
+++ src/app/models/habit.ts
@@ -0,0 +1,11 @@
+export class Habit {
+  name: string;
+  frequency: string;
+  description: string;
+
+  constructor() {
+    this.name = '';
+    this.frequency = '';
+    this.description = '';
+  }
+}
